66f474175388316a2ca286516680e350b7503a49
BrainStormGGG <alaparevich@gmail.com>
2022-09-17 00:19:58 +0300

    ss

--- settings/post/admin.py
+++ settings/post/admin.py
@@ -1,4 +1,18 @@
 from django.contrib import admin
 from .models import Post
+from django import forms
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
 
-admin.site.register(Post)
+
+
+class PostAdminForm(forms.ModelForm):
+    content = forms.CharField(widget=CKEditorUploadingWidget())
+    class Meta:
+        model = Post
+        fields = '__all__'
+
+
+class PostAdmin(admin.ModelAdmin):
+    form = PostAdminForm
+
+admin.site.register(Post, PostAdmin)
--- settings/settings/urls.py
+++ settings/settings/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('post.urls')),
+    path('ckeditor/', include('ckeditor_uploader.urls')),
 
 ]
